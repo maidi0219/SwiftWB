ff73e14aca261d7d23ff059ffaa6697a094d7419
lxl <jacy_li@kingdee.com>
2016-12-30 15:24:01 +0800

    000002

--- Swift微博/Swift微博/AppDelegate.swift
+++ Swift微博/Swift微博/AppDelegate.swift
@@ -13,8 +13,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
     var blockRotation: Bool = false
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        print("123");
         window = UIWindow(frame:UIScreen.main.bounds);
         window?.backgroundColor = UIColor.white;
         window?.rootViewController = WbMainTabarViewController();
